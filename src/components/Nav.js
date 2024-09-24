e3298bc03a8beeecb6397602c1dd518f7692ebac
hjoo830 <hjoo830@naver.com>
2024-09-25 02:55:21 +0900

    ✨ Feat: Change NavBar color when scrolling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Nav.css";
 
 export default function Nav() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 50) {
+        setShow(true);
+      } else {
+        setShow(false);
+      }
+    });
+    return () => window.removeEventListener("scroll", () => {});
+  }, []);
+
   return (
-    <nav className={`nav`}>
+    <nav className={`nav ${show && "nav__black"} `}>
       <img
         alt="Netflix logo"
         src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
