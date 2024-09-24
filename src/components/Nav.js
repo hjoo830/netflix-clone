f5de62a2aa52915df52369027d188e16846fa88f
hjoo830 <hjoo830@naver.com>
2024-09-25 02:54:20 +0900

    ✨ Feat: Nav UI

---
+++ src/components/Nav.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Nav.css";
+
+export default function Nav() {
+  return (
+    <nav className={`nav`}>
+      <img
+        alt="Netflix logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/170px-Netflix_2015_logo.svg.png"
+        className="nav__logo"
+        onClick={() => window.location.reload()}
+      />
+      <img
+        alt="User logged"
+        src="https://occ-0-4796-988.1.nflxso.net/dnm/api/v6/K6hjPJd6cR6FpVELC5Pd6ovHRSk/AAAABbme8JMz4rEKFJhtzpOKWFJ_6qX-0y5wwWyYvBhWS0VKFLa289dZ5zvRBggmFVWVPL2AAYE8xevD4jjLZjWumNo.png?r=a41"
+        className="nav__avatar"
+      />
+    </nav>
+  );
+}
