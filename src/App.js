c3b780dd722c5275894fa694057e5cf4977c9a81
hjoo830 <hjoo830@naver.com>
2024-09-25 03:32:21 +0900

    ✨ Feat: Banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from "./components/Nav";
+import Banner from "./components/Banner";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Nav />
+      <Banner />
     </div>
   );
 }
---
+++ src/components/Banner.js
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import axios from "../api/axios";
+import requests from "../api/requests";
+import "./Banner.css";
+
+export default function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const request = await axios.get(requests.fetchNowPlaying);
+    const movieId =
+      request.data.results[
+        Math.floor(Math.random() * request.data.results.length)
+      ].id;
+
+    const { data: movieDetail } = await axios.get(`movie/${movieId}`, {
+      params: { append_to_response: "videos" },
+    });
+    setMovie(movieDetail);
+  };
+
+  const truncate = (str, n) => {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  };
+
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        backgroundPosition: "top center",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        <div className="banner__buttons">
+          <button className="banner__button play">Play</button>
+          <button className="banner__button info">
+            <div className="space"></div>More Information
+          </button>
+        </div>
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 100)}
+        </h1>
+      </div>
+      <div className="banner__fadeBottom" />
+    </header>
+  );
+}
