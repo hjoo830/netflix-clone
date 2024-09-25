6cf6d7712eac041bfcd2f90f8ab7fd24677139c0
hjoo830 <hjoo830@naver.com>
2024-09-26 00:09:47 +0900

    ✨ Feat: Row component

---
+++ src/components/Row.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import axios from "../api/axios";
+import "./Row.css";
+
+const Row = ({ title, fetchUrl, isLargeRow, id }) => {
+  const [movies, setMovies] = useState([]);
+  const BASEURL = "https://image.tmdb.org/t/p/original";
+
+  useEffect(() => {
+    fetchMovieData();
+  }, [fetchUrl]);
+
+  const fetchMovieData = async () => {
+    const request = await axios.get(fetchUrl);
+    setMovies(request.data.results);
+    return request;
+  };
+
+  return (
+    <section className="row">
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span className="arrow">{"<"}</span>
+        </div>
+        <div id={id} className="row__posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`${BASEURL}${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              loading="lazy"
+              alt={movie.name}
+            />
+          ))}
+        </div>
+        <div className="slider__arrow-right">
+          <span className="arrow">{">"}</span>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Row;
