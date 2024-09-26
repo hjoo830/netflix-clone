d36acd09143b4d7cd76a939f4ea76ff65781cdba
hjoo830 <hjoo830@naver.com>
2024-09-27 03:57:31 +0900

    ✨ Feat: Search Page

---
+++ src/pages/SearchPage/index.js
@@ -0,0 +1,67 @@
+import axios from "../../api/axios";
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import "./SearchPage.css";
+
+export default function SearchPage() {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const useQuery = () => {
+    return new URLSearchParams(useLocation().search);
+  };
+
+  let query = useQuery();
+  const searchTerm = query.get("q");
+
+  useEffect(() => {
+    if (searchTerm) {
+      fetchSearchMovie(searchTerm);
+    }
+  }, [searchTerm]);
+
+  const fetchSearchMovie = async (searchTerm) => {
+    try {
+      const request = await axios.get(
+        `/search/multi?include_adult=false&query=${searchTerm}`
+      );
+      setSearchResults(request.data.results);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const renderSearchResults = () => {
+    return searchResults.length > 0 ? (
+      <section className="search-container">
+        {searchResults.map((movie) => {
+          if (movie.backdrop_path !== null && movie.media_type !== "person") {
+            const movieImageUrl =
+              "https://image.tmdb.org/t/p/w500" + movie.backdrop_path;
+            return (
+              <div className="movie">
+                <div className="movie__column-poster">
+                  <img
+                    src={movieImageUrl}
+                    alt="movie image"
+                    className="movie__poster"
+                  />
+                </div>
+              </div>
+            );
+          }
+        })}
+      </section>
+    ) : (
+      <section className="no-results">
+        <div className="no-results__text">
+          <p>
+            "{searchTerm}"과 일치하는 결과가 없습니다. 다른 검색어로
+            검색해주세요.
+          </p>
+        </div>
+      </section>
+    );
+  };
+
+  return renderSearchResults();
+}
