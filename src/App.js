c15038e86f70c4ec0aff811bdcf13ad5522e1945
hjoo830 <hjoo830@naver.com>
2024-09-27 03:19:14 +0900

    ✨ Feat: Set up routing

--- src/App.js
+++ src/App.js
@@ -1,45 +1,32 @@
-import Nav from "./components/Nav";
-import Banner from "./components/Banner";
-import Row from "./components/Row";
-import Footer from "./components/Footer";
-import requests from "./api/requests";
+import { Outlet, Routes, Route } from "react-router-dom";
 import "./App.css";
+
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
+import MainPage from "./pages/MainPage";
+import DetailPage from "./pages/DetailPage";
+import SearchPage from "./pages/SearchPage";
+
+const Layout = () => {
+  return (
+    <div>
+      <Nav />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Banner />
-
-      <Row
-        title="NETFLIX ORIGINALS"
-        id="NO"
-        fetchUrl={requests.fetchNetflixOriginals}
-        isLargeRow
-      />
-      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
-      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
-      <Row
-        title="Action Movies"
-        id="AM"
-        fetchUrl={requests.fetchActionMovies}
-      />
-      <Row
-        title="Comedy Movies"
-        id="CM"
-        fetchUrl={requests.fetchComedyMovies}
-      />
-      <Row
-        title="Romance Movies"
-        id="RM"
-        fetchUrl={requests.fetchRomanceMovies}
-      />
-      <Row
-        title="Documentaries"
-        id="DM"
-        fetchUrl={requests.fetchDocumentaries}
-      />
-      <Footer />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage />} />
+          <Route path=":movieId" element={<DetailPage />} />
+          <Route path="search" element={<SearchPage />} />
+        </Route>
+      </Routes>
     </div>
   );
 }
