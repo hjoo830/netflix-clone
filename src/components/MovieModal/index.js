1b3e95b1c3a36065f0a73ff18311e19b8e32f164
hjoo830 <hjoo830@naver.com>
2024-09-28 00:01:27 +0900

    ✨ Feat: close modal on outside click

--- src/components/MovieModal/index.js
+++ src/components/MovieModal/index.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./MovieModal.css";
+import useOnClickOutside from "../../hooks/useOnClickOutside";
 
 const MovieModal = ({
   backdrop_path,
@@ -12,10 +13,15 @@ const MovieModal = ({
   setModalOpen,
 }) => {
   const BASEURL = "https://image.tmdb.org/t/p/original";
+  const ref = useRef();
+  useOnClickOutside(ref, () => {
+    setModalOpen(false);
+  });
+
   return (
     <div className="presentation" role="presentation">
       <div className="wrapper-modal">
-        <div className="modal">
+        <div className="modal" ref={ref}>
           <span onClick={() => setModalOpen(false)} className="modal-close">
             X
           </span>
