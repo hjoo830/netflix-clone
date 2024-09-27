6e6f77da5d4fa1e20661b35314b2004f95a9845a
hjoo830 <hjoo830@naver.com>
2024-09-28 00:39:59 +0900

    ♻️ Refactor: move API key to .env file

--- src/api/axios.js
+++ src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   baseURL: "https://api.themoviedb.org/3",
   params: {
-    api_key: "61024ff31e64042803b31a91a081d80e",
+    api_key: process.env.REACT_APP_MOVIE_DB_API_KEY,
     language: "ko-KR",
   },
 });
