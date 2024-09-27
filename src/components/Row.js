4b79ad099dccaa791f0eb52c81911cb78ab3d199
hjoo830 <hjoo830@naver.com>
2024-09-28 00:26:59 +0900

    ✨ Feat: implement touch slide using Swiper

--- src/components/Row.js
+++ src/components/Row.js
@@ -3,6 +3,14 @@ import axios from "../api/axios";
 import MovieModal from "./MovieModal";
 import "./Row.css";
 
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
 const Row = ({ title, fetchUrl, isLargeRow, id }) => {
   const [movies, setMovies] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
@@ -27,38 +35,51 @@ const Row = ({ title, fetchUrl, isLargeRow, id }) => {
   return (
     <section className="row">
       <h2>{title}</h2>
-      <div className="slider">
-        <div
-          className="slider__arrow-left"
-          onClick={() => {
-            document.getElementById(id).scrollLeft -= window.innerWidth - 80;
-          }}
-        >
-          <span className="arrow">{"<"}</span>
-        </div>
+
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        loop={true}
+        navigation
+        pagination={{ clickable: true }}
+        breakpoints={{
+          1378: {
+            slidesPerView: 6,
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5,
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4,
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3,
+            slidesPerGroup: 3,
+          },
+        }}
+        resizeObserver={false}
+      >
         <div id={id} className="row__posters">
           {movies.map((movie) => (
-            <img
-              key={movie.id}
-              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
-              src={`${BASEURL}${
-                isLargeRow ? movie.poster_path : movie.backdrop_path
-              }`}
-              loading="lazy"
-              alt={movie.name}
-              onClick={() => handleClick(movie)}
-            />
+            <SwiperSlide>
+              <img
+                key={movie.id}
+                style={{ padding: "25px 0" }}
+                className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+                src={`${BASEURL}${
+                  isLargeRow ? movie.poster_path : movie.backdrop_path
+                }`}
+                loading="lazy"
+                alt={movie.name}
+                onClick={() => handleClick(movie)}
+              />
+            </SwiperSlide>
           ))}
         </div>
-        <div
-          className="slider__arrow-right"
-          onClick={() => {
-            document.getElementById(id).scrollLeft += window.innerWidth - 80;
-          }}
-        >
-          <span className="arrow">{">"}</span>
-        </div>
-      </div>
+      </Swiper>
+
       {modalOpen && (
         <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
       )}
